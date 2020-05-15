fa840608afe2828ce7aca6142efb851094a95d9c
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-15 23:39:37 +0300

    Whoa Cpmpleted this lab

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+let msg
+function greet(time){
+  let splitedTime = time.split(" ").join(':');
+
+  let intTime = parseInt(time);
+  console.log(intTime);
+  if (intTime < 12){
+     return "Good Morning";
+  }
+  if (intTime >= 12 && intTime < 17){
+    return "Good Afternoon";
+
+ }
+ if (intTime >= 17){
+  return "Good Evening";
+}
+}
 /* Write your implementation of displayMessage() */
+function displayMessage (greeting){
+  let msg = document.getElementById('greeting');
+  msg.innerHTML = greeting;
+
+
+
+}
